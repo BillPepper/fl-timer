b1abf8e7de5ecfec1e11be818ad90e12521f3635
Mattes Wenzel <mattes@flawlesslogic.net>
2024-09-24 01:20:35 +0200

    Incrememt version

--- src/timer.ts
+++ src/timer.ts
@@ -5,7 +5,7 @@ import { FLTimer } from "./interfaces";
 
 import "./style.scss";
 
-const version = "0.3.2";
+const version = "0.3.3";
 const versionString = document.getElementById("version-string");
 versionString.innerText = `v${version} // This tools does not save any data.`;
 const alarmSound = new Audio('./alarm.mp3')
